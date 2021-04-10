03fd5f50f28c375bb8bc0c456d27798f142aa793
Eduardo Roseo <eduardoroseo@gmail.com>
2021-04-09 23:04:46 -0300

    auth and user configuration

---
+++ app/Http/Response/ApiResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Http\Response;
+
+
+use Illuminate\Http\JsonResponse;
+
+class ApiResponse
+{
+    public static function error(?string $message, array $errors = [], int $http_code = 400): JsonResponse
+    {
+        return response()->json([
+            'isOk' => false,
+            'message' => $message,
+            'errors' => $errors
+        ], $http_code);
+    }
+
+    public static function success(array $data = [], int $http_code = 200): JsonResponse
+    {
+        return response()->json([
+            'isOk' => true,
+            'content' => $data
+        ], $http_code);
+    }
+}
